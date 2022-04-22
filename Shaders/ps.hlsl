390591f3f372fa02bddb364a3244bf2924dbdf59
Ceffa93 <ceffa93@gmail.com>
2022-04-22 20:12:46 +0900

    Cleaned build warning

--- Shaders/ps.hlsl
+++ Shaders/ps.hlsl
@@ -10,7 +10,7 @@ float4 PSMain(PSInput input) : SV_TARGET
 	bool left = input.uv.x < .5;
 
 	float2 uv = float2(frac(input.uv.x * 2), input.uv.y);
-
+	
 	if (left) return g_texture2D[TEX2D_SRV].Sample(g_sampler[0], uv);
 	else return g_texture3D[TEX3D_SRV].Sample(g_sampler[0], float3(uv,1));
 }
